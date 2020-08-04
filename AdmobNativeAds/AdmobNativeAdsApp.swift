c5864eb53a10bb7fffaa9d3e97968e2c679f8884
Yuri Ramocan <yramocan@umich.edu>
2020-08-04 01:03:54 -0400

    Initial Commit

---
+++ AdmobNativeAds/AdmobNativeAdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdmobNativeAdsApp.swift
+//  AdmobNativeAds
+//
+//  Created by Yuri Ramocan on 8/4/20.
+//
+
+import SwiftUI
+
+@main
+struct AdmobNativeAdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdmobNativeAds/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  AdmobNativeAds
+//
+//  Created by Yuri Ramocan on 8/4/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
